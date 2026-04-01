588adec3ae104d9d97606d68bec7ee093464175e
Gonzalo <goladen@gmail.com>
2026-04-01 20:49:12 +0200

    new

--- public/kartingedmulti/Build/kartingedmulti.loader.js
+++ public/kartingedmulti/Build/kartingedmulti.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 65493608,
+    wasmFileSize: 65494835,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },

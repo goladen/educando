a33be8f5d3a3ddb1d55060efde4c829f4ffb8d0d
Gonzalo <goladen@gmail.com>
2026-05-08 13:22:28 +0200

    0805261322

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,18 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 3000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor-three': ['three'],
+          'vendor-react': ['react', 'react-dom'],
+          'vendor-firebase': ['firebase/app', 'firebase/firestore', 'firebase/auth', 'firebase/storage'],
+        },
+      },
+    },
+  },
   plugins: [
     react(),
     VitePWA({
